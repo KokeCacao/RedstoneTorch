31f8d3ad6c40dee268b7557af6fc655460e53402
Hanke Chen <i@kokecacao.me>
2018-10-01 23:02:23 -0400

    :+1: Free Resnet pretrained

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -1,4 +1,5 @@
 # full assembly of the sub-parts to form the complete net
+import torchvision
 
 from .unet_parts import *
 
@@ -19,30 +20,158 @@ class UNet(nn.Module):
         self.outc = outconv(64, n_classes)
 
     def forward(self, x, depth):
-        print("x size = {}".format(x.size()))
+        # print("x size = {}".format(x.size())) [32, 3, 224, 224]
         x1 = self.inc(x)
-        print("x1 size = {}".format(x1.size()))
+        # print("x1 size = {}".format(x1.size())) [32, 64, 224, 224]
         x2 = self.down1(x1)
-        print("x2 size = {}".format(x2.size()))
+        # print("x2 size = {}".format(x2.size())) [32, 128, 112, 112]
         x3 = self.down2(x2)
-        print("x3 size = {}".format(x3.size()))
+        # print("x3 size = {}".format(x3.size())) [32, 256, 56, 56]
         x4 = self.down3(x3)
-        print("x4 size = {}".format(x4.size()))
+        # print("x4 size = {}".format(x4.size())) [32, 512, 28, 28]
 
         # add depth data here
         join = self.join(x4, depth)
-        print("join size = {}".format(join.size()))
+        # print("join size = {}".format(join.size())) [32, 512, 28, 28]
 
         x5 = self.down4(join)
-        print("x5 size = {}".format(x5.size()))
+        # print("x5 size = {}".format(x5.size())) [32, 512, 14, 14]
         x = self.up1(x5, x4)
-        print("after up1 size = {}".format(x.size()))
+        # print("after up1 size = {}".format(x.size())) [32, 256, 28, 28]
         x = self.up2(x, x3)
-        print("after up2 size = {}".format(x.size()))
+        # print("after up2 size = {}".format(x.size())) [32, 128, 56, 56]
         x = self.up3(x, x2)
-        print("after up3 size = {}".format(x.size()))
+        # print("after up3 size = {}".format(x.size())) [32, 64, 112, 112]
         x = self.up4(x, x1)
-        print("after up4 size = {}".format(x.size()))
+        # print("after up4 size = {}".format(x.size())) [32, 64, 224, 224]
         x = self.outc(x)
-        print("outc size = {}".format(x.size()))
+        # print("outc size = {}".format(x.size())) [32, 1, 224, 224]
+        return x
+
+class UNetResNet(nn.Module):
+    """PyTorch U-Net model using ResNet(34, 101 or 152) encoder.
+
+    UNet: https://arxiv.org/abs/1505.04597
+    ResNet: https://arxiv.org/abs/1512.03385
+    Proposed by Alexander Buslaev: https://www.linkedin.com/in/al-buslaev/
+
+    Args:
+            encoder_depth (int): Depth of a ResNet encoder (34, 101 or 152).
+            num_classes (int): Number of output classes.
+            num_filters (int, optional): Number of filters in the last layer of decoder. Defaults to 32.
+            dropout_2d (float, optional): Probability factor of dropout layer before output layer. Defaults to 0.2.
+            pretrained (bool, optional):
+                False - no pre-trained weights are being used.
+                True  - ResNet encoder is pre-trained on ImageNet.
+                Defaults to False.
+            is_deconv (bool, optional):
+                False: bilinear interpolation is used in decoder.
+                True: deconvolution is used in decoder.
+                Defaults to False.
+
+    """
+
+    def __init__(self, encoder_depth, num_classes, num_filters=32, dropout_2d=0.2,
+                 pretrained=False, is_deconv=False):
+        super().__init__()
+        self.num_classes = num_classes
+        self.dropout_2d = dropout_2d
+
+        if encoder_depth == 34:
+            self.encoder = torchvision.models.resnet34(pretrained=pretrained)
+            bottom_channel_nr = 512
+        elif encoder_depth == 101:
+            self.encoder = torchvision.models.resnet101(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        elif encoder_depth == 152:
+            self.encoder = torchvision.models.resnet152(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        else:
+            raise NotImplementedError('only 34, 101, 152 version of Resnet are implemented')
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv1 = nn.Sequential(self.encoder.conv1,
+                                   self.encoder.bn1,
+                                   self.encoder.relu,
+                                   self.pool)
+
+        self.conv2 = self.encoder.layer1
+
+        self.conv3 = self.encoder.layer2
+
+        self.conv4 = self.encoder.layer3
+
+        self.conv5 = self.encoder.layer4
+
+        self.center = DecoderBlockV2(bottom_channel_nr, num_filters * 8 * 2, num_filters * 8, is_deconv)
+        self.dec5 = DecoderBlockV2(bottom_channel_nr + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv)
+        self.dec4 = DecoderBlockV2(bottom_channel_nr // 2 + num_filters * 8, num_filters * 8 * 2, num_filters * 8,
+                                   is_deconv)
+        self.dec3 = DecoderBlockV2(bottom_channel_nr // 4 + num_filters * 8, num_filters * 4 * 2, num_filters * 2,
+                                   is_deconv)
+        self.dec2 = DecoderBlockV2(bottom_channel_nr // 8 + num_filters * 2, num_filters * 2 * 2, num_filters * 2 * 2,
+                                   is_deconv)
+        self.dec1 = DecoderBlockV2(num_filters * 2 * 2, num_filters * 2 * 2, num_filters, is_deconv)
+        self.dec0 = ConvRelu(num_filters, num_filters)
+        self.final = nn.Conv2d(num_filters, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        conv1 = self.conv1(x)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        conv4 = self.conv4(conv3)
+        conv5 = self.conv5(conv4)
+
+        pool = self.pool(conv5)
+        center = self.center(pool)
+
+        dec5 = self.dec5(torch.cat([center, conv5], 1))
+
+        dec4 = self.dec4(torch.cat([dec5, conv4], 1))
+        dec3 = self.dec3(torch.cat([dec4, conv3], 1))
+        dec2 = self.dec2(torch.cat([dec3, conv2], 1))
+        dec1 = self.dec1(dec2)
+        dec0 = self.dec0(dec1)
+
+        return self.final(F.dropout2d(dec0, p=self.dropout_2d))
+
+class DecoderBlockV2(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, is_deconv=True):
+        super(DecoderBlockV2, self).__init__()
+        self.in_channels = in_channels
+
+        if is_deconv:
+            """
+                Paramaters for Deconvolution were chosen to avoid artifacts, following
+                link https://distill.pub/2016/deconv-checkerboard/
+            """
+
+            self.block = nn.Sequential(
+                ConvRelu(in_channels, middle_channels),
+                nn.ConvTranspose2d(middle_channels, out_channels, kernel_size=4, stride=2,
+                                   padding=1),
+                nn.ReLU(inplace=True)
+            )
+        else:
+            self.block = nn.Sequential(
+                nn.Upsample(scale_factor=2, mode='bilinear'),
+                ConvRelu(in_channels, middle_channels),
+                ConvRelu(middle_channels, out_channels),
+            )
+
+    def forward(self, x):
+        return self.block(x)
+
+class ConvRelu(nn.Module):
+    def __init__(self, in_, out):
+        super().__init__()
+        self.conv = conv3x3(in_, out)
+        self.activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.activation(x)
         return x
