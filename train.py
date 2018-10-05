3eb366f76b2858a956ade22c11fbf1dc2c9b7a78
Hanke Chen <i@kokecacao.me>
2018-10-04 20:20:37 -0400

    :bug: trying to convert to nparray and then to tensor

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import os
 from optparse import OptionParser
 
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.utils.data as data
 from torch import optim
@@ -84,8 +85,8 @@ def train_net(net,
     if args.visualization:
         visual_id = tgs_data.get_data()['id'][:10]
         # visual_z = tgs_data.get_data()['z'][:10].float()
-        visual_image = torch.from_numpy(tgs_data.get_data()['image'][:10])
-        visual_mask = torch.from_numpy(tgs_data.get_data()['mask'][:10])
+        visual_image = torch.from_numpy(np.array(tgs_data.get_data()['image'][:10]))
+        visual_mask = torch.from_numpy(np.array(tgs_data.get_data()['mask'][:10]))
         writer.add_embedding(visual_image.view(10), metadata="image_"+visual_id, label_img=visual_image.unsqueeze(1))
         writer.add_embedding(visual_mask.view(10), metadata="mask_"+visual_id, label_img=visual_mask.unsqueeze(1))
 
