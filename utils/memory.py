db05544de4884c781e5318a93e9b04a4a0bdcc19
Hanke Chen <i@kokecacao.me>
2018-10-07 11:39:47 -0400

    :bug: add while loop

--- utils/memory.py
+++ utils/memory.py
@@ -17,14 +17,15 @@ class memory_thread(threading.Thread):
         self.count = 0
 
     def run(self):
-        if self.gpu != "":
-            torch.cuda.empty_cache()  # release gpu memory
-            for key, value in get_gpu_memory_map().items():
-                self.writer.add_scalars('memory/GPU', {"GPU-" + str(key): value}, self.count)
-        self.writer.add_scalars('memory/CPU', {"CPU Usage": psutil.cpu_percent()}, self.count)
-        # if psutil.virtual_memory() != None: self.writer.add_scalars('memory/Physical', {"Physical_Mem Usage": psutil.virtual_memory()}, self.count)
-        self.count = self.count + 1
-        time.sleep(1)
+        while(True):
+            if self.gpu != "":
+                torch.cuda.empty_cache()  # release gpu memory
+                for key, value in get_gpu_memory_map().items():
+                    self.writer.add_scalars('memory/GPU', {"GPU-" + str(key): value}, self.count)
+            self.writer.add_scalars('memory/CPU', {"CPU Usage": psutil.cpu_percent()}, self.count)
+            # if psutil.virtual_memory() != None: self.writer.add_scalars('memory/Physical', {"Physical_Mem Usage": psutil.virtual_memory()}, self.count)
+            self.count = self.count + 1
+            time.sleep(1)
 
 def get_gpu_memory_map():
     """Get the current gpu usage.
