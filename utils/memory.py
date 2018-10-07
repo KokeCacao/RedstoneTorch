76a5e36e8c1bd69ed01d70d5124f45e8ca124719
Hanke Chen <i@kokecacao.me>
2018-10-07 11:14:24 -0400

    :+1: see all memory in another thread and log

---
+++ utils/memory.py
@@ -0,0 +1,46 @@
+import threading
+import time
+
+import torch
+import psutil
+import subprocess
+
+exitFlag = 0
+
+
+class memory_thread(threading.Thread):  # 继承父类threading.Thread
+    def __init__(self, threadID, writer, gpu):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.writer = writer
+        self.gpu = gpu
+        self.count = 0
+
+    def run(self):
+        if self.gpu != "":
+            torch.cuda.empty_cache()  # release gpu memory
+            for key, value in get_gpu_memory_map():
+                self.writer.add_scalars('memory/GPU', {"GPU-" + str(key): value}, self.count)
+        self.writer.add_scalars('memory/CPU', {"CPU Usage": psutil.cpu_percent()}, self.count)
+        self.writer.add_scalars('memory/Physical', {"Physical_Mem Usage": psutil.virtual_memory()}, self.count)
+        self.count = self.count + 1
+        time.sleep(1)
+
+def get_gpu_memory_map():
+    """Get the current gpu usage.
+
+    Returns
+    -------
+    usage: dict
+        Keys are device ids as integers.
+        Values are memory usage as integers in MB.
+    """
+    result = subprocess.check_output(
+        [
+            'nvidia-smi', '--query-gpu=memory.used',
+            '--format=csv,nounits,noheader'
+        ], encoding='utf-8')
+    # Convert lines into a dictionary
+    gpu_memory = [int(x) for x in result.strip().split('\n')]
+    gpu_memory_map = dict(zip(range(len(gpu_memory)), gpu_memory))
+    return gpu_memory_map
