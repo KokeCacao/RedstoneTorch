c5dc340db81410d34eeeb6f9dfa9eb460af2bd17
Hanke Chen <i@kokecacao.me>
2018-10-06 22:35:38 -0400

    :bug: use detach() instead of converting to np

--- eval.py
+++ eval.py
@@ -60,7 +60,7 @@ def eval_net(net, validation_loader, gpu=False, visualization=False, writer=None
 
 
                 plt.subplot(224)
-                plt.imshow(tensor_to_PIL(np.abs(np.array(true_mask[index])-np.array(masks_pred[index]))))
+                plt.imshow(tensor_to_PIL(np.abs(np.array(true_mask[index].detach())-np.array(masks_pred[index].detach()))))
                 plt.title("Error")
                 plt.grid(True)
                 writer.add_figure("image/epoch_validation/"+str(index), F, global_step=global_plot_step, close=False, walltime=None)
