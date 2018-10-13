a64c70d0faf93db1d5575f38c4ec6116f57e5d08
Hanke Chen <i@kokecacao.me>
2018-10-12 23:51:40 -0400

    :bug:

--- eval.py
+++ eval.py
@@ -19,14 +19,16 @@ def eval_net(net, validation_loader, dataset, gpu, visualization, writer, epoch_
     total_iou = 0
     for batch_index, (id, z, image_0, true_mask_0) in enumerate(validation_loader, 0):
 
+        # https://imgaug.readthedocs.io/en/latest/source/examples_segmentation_maps.html#a-simple-example
         seq_det = config.TRAIN_SEQUENCE.to_deterministic()
-        image = seq_det.augment_batches(tensor_to_PIL(image_0))
-        true_mask = seq_det.augment_batches(tensor_to_PIL(true_mask_0))
+        image = seq_det.augment_images(image_0.numpy().astype('uint8'))
+        true_mask = seq_det.augment_images(true_mask_0.numpy().astype('uint8'))
 
-        # image = image.unsqueeze(0)
-        # true_mask = true_mask.unsqueeze(0)
+        if config.TRAIN_TRASNFORM:
+            image = config.TRAIN_TRASNFORM['image'](image)
+            true_mask = config.TRAIN_TRASNFORM['mask'](true_mask)
 
-        if gpu != "": #trying to use cuda 1 to prevent out of memory
+        if gpu != "":
             # z = z.cuda()
             image = image.cuda()
             true_mask = true_mask.cuda()
