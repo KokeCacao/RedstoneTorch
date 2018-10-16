6c0e4c95bf81a87c01299b546b8e64e8bb55ed9e
Hanke Chen <i@kokecacao.me>
2018-10-16 19:40:27 -0400

    :bug: debug None Type, transfer Tensor to Numpy Array so that I can use '=='

--- eval.py
+++ eval.py
@@ -116,13 +116,13 @@ def iou_score(outputs, labels, threshold=0.5):
     # You can comment out this line if you are passing tensors of equal shape
     # But if you are passing output from UNet or something it will most probably
     # be with the BATCH x 1 x H x W shape
-    outputs = outputs.squeeze(1).byte().cpu()  # BATCH x 1 x H x W => BATCH x H x W
-    labels = labels.squeeze(1).byte().cpu()
+    outputs = outputs.squeeze(1).byte()  # BATCH x 1 x H x W => BATCH x H x W
+    labels = labels.squeeze(1).byte()
 
     intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
     union = (outputs | labels).float().sum((1, 2))  # Will be zero if both are 0
 
-    iou = (intersection + 1e-10) / (union + 1e-10)  # We smooth our devision to avoid 0/0
+    iou = np.array((intersection + 1e-10) / (union + 1e-10))  # We smooth our devision to avoid 0/0
 
     # thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
     if iou == None:
