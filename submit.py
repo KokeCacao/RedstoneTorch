b2ae526df0e764a713e5d3f8310ba994d4bfec36
Hanke Chen <i@kokecacao.me>
2018-10-18 08:29:58 -0400

    :bug: strange, try another encoding method

--- submit.py
+++ submit.py
@@ -105,7 +105,7 @@ def rle_encode(img):
     pixels = np.concatenate(([0], pixels, [0]))
     runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
     runs[1::2] -= runs[::2]
-    return ' '.join(str(x) for x in runs))
+    return ' '.join(str(x) for x in runs)
 
 # credits to https://stackoverflow.com/users/6076729/manuel-lagunas
 # def rle_encode(mask_image):
