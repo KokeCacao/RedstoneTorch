962d5f089b3385535f37935a73a208bda449ff10
Hanke Chen <i@kokecacao.me>
2018-10-28 12:35:37 -0400

    :bug: fix float32 conversion

--- dataset/hpa_dataset.py
+++ dataset/hpa_dataset.py
@@ -160,7 +160,7 @@ class HPAData(data.Dataset):
         :param id: id
         :return: one hot encoded label
         """
-        return self.one_hot_frame[id].astype(np.float32)
+        return np.float32(self.one_hot_frame[id])
 
 
 class TrainImgAugTransform:
