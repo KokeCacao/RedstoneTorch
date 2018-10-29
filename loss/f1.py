e4c8e41618238eef6172c0e09e95eb911c4270ce
Hanke Chen <i@kokecacao.me>
2018-10-29 03:11:27 -0400

    :bug: fix turn tensor to bit

--- loss/f1.py
+++ loss/f1.py
@@ -18,7 +18,7 @@ def competitionMetric(predicted, label, threshold = 0.5, epsilon = 1e-8):
     :param epsilon: small number
     :return: scaler: (2 * precision * recall) / (precision + recall + epsilon)
     """
-    predicted = (predicted > threshold).astype(np.float32)
+    predicted = (predicted > threshold).float()
 
     #f1 per feature
     groundPositives = np.sum(label, axis=0) + epsilon
@@ -31,3 +31,26 @@ def competitionMetric(predicted, label, threshold = 0.5, epsilon = 1e-8):
     m = (2 * precision * recall) / (precision + recall + epsilon)
 
     return m.mean()
+
+
+def f1_micro(y_true, y_preds, thresh=0.5, eps=1e-20):
+    preds_bin = y_preds > thresh  # binary representation from probabilities (not relevant)
+    truepos = preds_bin * y_true
+
+    p = truepos.sum() / (preds_bin.sum() + eps)  # take sums and calculate precision on scalars
+    r = truepos.sum() / (y_true.sum() + eps)  # take sums and calculate recall on scalars
+
+    f1 = 2 * p * r / (p + r + eps)  # we calculate f1 on scalars
+    return f1
+
+
+def f1_macro(y_true, y_preds, thresh=0.5, eps=1e-20):
+    preds_bin = y_preds > thresh  # binary representation from probabilities (not relevant)
+    truepos = preds_bin * y_true
+
+    p = truepos.sum(axis=0) / (preds_bin.sum(axis=0) + eps)  # sum along axis=0 (classes)
+    # and calculate precision array
+    r = truepos.sum(axis=0) / (y_true.sum(axis=0) + eps)  # sum along axis=0 (classes)
+    #  and calculate recall array
+
+    f1 = 2 * p * r / (p + r + eps)  # we calculate f1 on arrays
