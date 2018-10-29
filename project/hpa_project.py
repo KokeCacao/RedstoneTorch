3948cabc6e315a9efd14510d23f08fd23d0b53df
Hanke Chen <i@kokecacao.me>
2018-10-29 03:27:02 -0400

    :bug: fix turn tensor to bit

--- project/hpa_project.py
+++ project/hpa_project.py
@@ -151,7 +151,6 @@ class HPAProject:
 
             """OUTPUT"""
             predict = predict.detach().cpu().numpy()
-            labels_0 = predict.detach().cpu().numpy()
             f1 = competitionMetric(predict, labels_0)
             train_duration = self.fold_begin - self.train_begin
             epoch_duration = self.fold_begin - self.epoch_begin
