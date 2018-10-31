:bug: fix focal git preserve gradient

loss/focal.py
@@ -45,8 +45,8 @@ class Focal_Loss_from_git(nn.Module):
 
         # Both reduce_sum and reduce_max are ok
         reduce_fl = fl.sum(dim=1)
-
-        return reduce_fl
+        print(reduce_fl)
+        return reduce_fl.sum()
 
 class FocalLoss0(nn.Module):
     def __init__(self, gamma=2):

project/hpa_project.py
@@ -187,7 +187,7 @@ class HPAProject:
                 image = image.cuda()
                 labels_0 = labels_0.cuda()
             predict = net(image)
-            predict = torch.nn.Softmax()(predict)
+            predict = torch.nn.Softmax(dim=1)(predict) # TODO: dim really = 1?
 
             """LOSS"""
             loss = Focal_Loss_from_git(alpha=0.25, gamma=2, eps=1e-7)(labels_0, predict)