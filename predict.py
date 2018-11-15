376a538d3532fe080b776eecefc42fc411ef897c
Hanke Chen <i@kokecacao.me>
2018-11-15 14:02:51 -0500

    :bug: corrected the path for prediction

--- predict.py
+++ predict.py
@@ -17,7 +17,7 @@ from utils.memory import memory_thread
 def get_args():
     parser = OptionParser()
     parser.add_option('--loaddir', dest='loaddir', default=False, help='tag you want to load')
-    parser.add_option('--versiontag', dest='versiontag', default="", help='tag for tensorboard-log')
+    parser.add_option('--versiontag', dest='versiontag', default="predict", help='tag for tensorboard-log and prediction')
     parser.add_option('--loadfile', dest='loadfile', default=False, help='file you want to load')
 
     (options, args) = parser.parse_args()
@@ -29,7 +29,7 @@ def load_args():
     if args.versiontag: config.PREDICTION_TAG = args.versiontag
     if args.loadfile:
         config.DIRECTORY_LOAD = config.DIRECTORY_PREFIX + "model/" + args.loaddir + "/" + args.loadfile
-        config.DIRECTORY_CHECKPOINT = config.DIRECTORY_PREFIX + "model/" + config.PROJECT_TAG + "/"
+        config.DIRECTORY_CHECKPOINT = config.DIRECTORY_PREFIX + "model/" + args.loaddir + "/predict/"
     else: raise ValueError("You must set --loadfile directory in prediction mode")
 
 
