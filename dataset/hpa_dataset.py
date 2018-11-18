864d1708f27821f6d1bf216afe521095aad6a1ee
Hanke Chen <i@kokecacao.me>
2018-11-18 13:56:10 -0500

    :bug: grammar

--- dataset/hpa_dataset.py
+++ dataset/hpa_dataset.py
@@ -568,6 +568,7 @@ def transform(ids, image_0, labels_0, train, val):
             Normalize(mean=[0.07459783, 0.05063238, 0.05089102, 0.07628681], std=[1, 1, 1, 1]),
         ])
         import pdb; pdb.set_trace()
+        print(image_0.shape)
         image = TRAIN_TRANSFORM(image_0)
         return (ids, image, labels_0, transforms.ToTensor()(image_0))
     elif not train and val:
