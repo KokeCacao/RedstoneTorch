:bug: fix softmax

loss/f1.py
@@ -108,7 +108,7 @@ class differenciable_f1_softmax(nn.Module):
 
     def forward(self, labels, logits):
         batch_size = logits.size()[0]
-        p = torch.nn.Softmax(logits)
+        p = torch.nn.Softmax()(logits)
         l = labels
         num_pos = torch.sum(p, 1) + self.eps
         num_pos_hat = torch.sum(l, 1) + self.eps

project/qubo_project/qubo_train.py
@@ -241,7 +241,7 @@ class QUBOTrain:
                 image = image.cuda()
                 labels_0 = labels_0.cuda()
             logits_predict = net(image)
-            prob_predict = torch.nn.Softmax(logits_predict)
+            prob_predict = torch.nn.Softmax()(logits_predict)
 
             """LOSS"""
             focal = focalloss_softmax(alpha=0.25, gamma=5, eps=1e-7)(labels_0, logits_predict)
@@ -399,7 +399,7 @@ class QUBOEvaluation:
 
                 logits_predict = net(image)
                 if config.TRAIN_GPU_ARG: torch.cuda.empty_cache()
-                prob_predict = torch.nn.Softmax(logits_predict)
+                prob_predict = torch.nn.Softmax()(logits_predict)
 
                 """LOSS"""
                 focal = focalloss_softmax(alpha=0.25, gamma=5, eps=1e-7)(labels_0, logits_predict)