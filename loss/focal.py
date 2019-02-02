:bug: softmax should with dim=1

loss/focal.py
@@ -100,7 +100,7 @@ class focalloss_softmax(nn.Module):
         # gamma = 2
 
         # softmax layer
-        y_pred = torch.nn.Softmax(dim=0)(y_pred) # TODO: dim really = 1?
+        y_pred = torch.nn.Softmax(dim=1)(y_pred) # TODO: dim really = 1?
 
         # To avoid divided by zero
         y_pred = y_pred + self.eps

project/qubo_project/qubo_train.py
@@ -175,7 +175,7 @@ class QUBOTrain:
         except KeyboardInterrupt as e:
             print(e)
             self.writer.close()
-            print("To Resume: python train.py --versiontag 'test' --projecttag " + config.PROJECT_TAG + "--loadfile " + config.lastsave)
+            print("To Resume: python train.py --versiontag 'test' --projecttag " + config.PROJECT_TAG + " --loadfile " + config.lastsave)
             try:
                 sys.exit(0)
             except SystemExit:
@@ -229,7 +229,7 @@ class QUBOTrain:
         report = classification_report(np.amax(evaluation.epoch_label, axis=1), np.amax(evaluation.epoch_pred, axis=1), target_names=["gate", "bins", "buoy", "empty", "torpedo"])
         print(report)
         tensorboardwriter.write_text(self.writer, report, config.epoch)
-        
+
         max_names = max(f1_dict.items(), key=operator.itemgetter(1))
         min_names = min(f1_dict.items(), key=operator.itemgetter(1))
         print("""