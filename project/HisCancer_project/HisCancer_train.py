:bug: trying to fix False gradient.

project/HisCancer_project/HisCancer_train.py
@@ -52,9 +52,10 @@ class HisCancerTrain:
 
                 for c in net.children():
                     for child_counter, child in enumerate(c.children()):
-                        if child_counter not in config.MODEL_NO_GRAD: continue
-                        for paras in child.parameters():
-                            paras.requires_grad = False
+                        if child_counter in config.MODEL_NO_GRAD:
+                            print("Disable Gradient for child_counter: {}".format(child_counter))
+                            for paras in child.parameters():
+                                paras.requires_grad = False
 
                 if config.TRAIN_GPU_ARG: net = torch.nn.DataParallel(net, device_ids=config.TRAIN_GPU_LIST)
 