fc40b5e660e1a988b9a0d467cb209e655b50464d
Hanke Chen <i@kokecacao.me>
2019-02-26 01:02:36 -0500

    :bug: float to int

--- project/HisCancer_project/HisCancer_net.py
+++ project/HisCancer_project/HisCancer_net.py
@@ -136,9 +136,9 @@ class SEResNeXtBottleneck(Bottleneck):
         super(SEResNeXtBottleneck, self).__init__()
 
         # https://www.jianshu.com/p/749439fb026d
-        width = math.floor(planes * (base_width // 64)) * groups
+        width = int(math.floor(planes * (base_width / 64)) * groups)
         # width = math.floor(planes * (base_width / 64)) * groups
-        
+
         self.conv1 = nn.Conv2d(inplanes, width, kernel_size=1, bias=False,
                                stride=1)
         self.bn1 = nn.BatchNorm2d(width)
