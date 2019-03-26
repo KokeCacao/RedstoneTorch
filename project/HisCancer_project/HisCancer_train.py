a929e22c59734892203bf4e1c90c02e937803ab2
Hanke Chen <i@kokecacao.me>
2019-03-26 08:12:23 -0400

    :bug: fix shakeup

--- project/HisCancer_project/HisCancer_train.py
+++ project/HisCancer_project/HisCancer_train.py
@@ -299,12 +299,12 @@ class HisCancerTrain:
         shakeup = dict()
         pbar = tqdm(range(100))
         for i in pbar:
-            public_lb = set(np.random.choice(range(int(len(evaluation.epoch_pred)*0.5)), int(len(evaluation.epoch_pred)*0.5), replace=False))
+            public_lb = set(np.random.choice(range(len(evaluation.epoch_pred)), int(len(evaluation.epoch_pred)*0.5), replace=False))
             private_lb = set(range(len(evaluation.epoch_pred)))-public_lb
-            public_lb = np.array(public_lb)
-            public_lb = metrics.roc_auc_score(np.array(evaluation.epoch_label)[public_lb], np.array(evaluation.epoch_pred)[public_lb])
-            private_lb = np.array(private_lb)
-            private_lb = metrics.roc_auc_score(np.array(evaluation.epoch_label)[private_lb], np.array(evaluation.epoch_pred)[private_lb])
+            public_lb = np.array(list(public_lb)).astype(dtype=np.int)
+            public_lb = metrics.roc_auc_score(evaluation.epoch_label[public_lb], evaluation.epoch_pred[public_lb])
+            private_lb = np.array(list(private_lb)).astype(dtype=np.int)
+            private_lb = metrics.roc_auc_score(evaluation.epoch_label[private_lb], evaluation.epoch_pred[private_lb])
             score_diff = private_lb-public_lb
             shakeup[score_diff] = (public_lb, private_lb)
             pbar.set_description_str("Public LB: {}, Private LB: {}, Difference: {}".format(public_lb, private_lb, score_diff))
