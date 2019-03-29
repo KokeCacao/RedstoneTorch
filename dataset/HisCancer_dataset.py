69cb49bcad0c4fe3b0f7f6e4938a2c51fe60a52b
Hanke Chen <i@kokecacao.me>
2019-03-29 15:54:48 -0400

    :tada: start predudo

--- dataset/HisCancer_dataset.py
+++ dataset/HisCancer_dataset.py
@@ -59,7 +59,7 @@ class HisCancerDataset(data.Dataset):
             self.presudo_dataframe = pd.read_csv(config.DIRECTORY_PRESUDO_CSV, delimiter=',', encoding="utf-8-sig", engine='python').set_index('Id')
             for index in self.presudo_dataframe.index.tolist():
                 probability = self.presudo_dataframe.Label[index]
-                id.append('data/HisCancer_dataset/test/'+index)
+                id.append('data/HisCancer_dataset/test/'+index+'.npy')
                 self.labelframe.append([1-probability, probability])
 
         elif self.load_strategy == "test" or self.load_strategy == "predict":
