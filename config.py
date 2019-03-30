b7a86b3ae46731fc4792f005d7ba7f5ba7e183aa
Hanke Chen <i@kokecacao.me>
2019-03-30 08:10:20 -0400

    :tada: close weight decay, 1 eta for fast

--- config.py
+++ config.py
@@ -197,7 +197,7 @@ elif PROJECT_NAME == "HisCancer":
     # TRAIN_TRY_LR = False
     # TRAIN_TRY_LR_FORMULA = lambda x: x / (8 * np.mod(-x - 1, 600) + 0.1) - 0.000207 * x  # y=x/(8*\operatorname{mod}(-x-1,600)+0.1)-0.000207*x
     TRAIN_RATIO = 1
-    EVAL_RATIO = 1 # to 8 when needed
+    EVAL_RATIO = 4 # to 8 when needed
     FIND_LR_ON_VALIDATION = False
     FIND_LR_RATIO = 20 if FIND_LR_ON_VALIDATION else 100
 
