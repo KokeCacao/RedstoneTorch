b7d387c400d9048a383ac4051db6a3024d05bfa8
Hanke Chen <i@kokecacao.me>
2019-07-08 09:47:11 -0400

    :tada: add ce score; only back_prop ce; add dummy task; change to dice only; tweak loss

--- project/siim_project/siim_train.py
+++ project/siim_project/siim_train.py
@@ -343,8 +343,7 @@ class SIIMTrain:
                 iou = denoised_siim_dice(threshold=config.EVAL_THRESHOLD, iou=True, denoised=False)(labels, logits_predict)
                 hinge = lovasz_hinge(labels.squeeze(1), logits_predict.squeeze(1))
                 bce = BCELoss()(prob_empty, empty)
-                print(logits_predict.shape, labels.squeeze(1).shape)
-                ce = CrossEntropyLoss()(logits_predict.squeeze(1).view(logits_predict.shape[0], -1), labels.squeeze(1).view(labels.shape[0], -1).long())
+                ce = BCELoss()(logits_predict.squeeze(1).view(logits_predict.shape[0], -1), labels.squeeze(1).view(labels.shape[0], -1).long())
                 # loss = 0.5 * dice.mean() + 0.5 * bce.mean()
                 loss = ce.mean()
 
