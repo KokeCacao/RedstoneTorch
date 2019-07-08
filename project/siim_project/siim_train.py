:tada: fix backprop probability; add ce loss; fix lr_finder

project/siim_project/siim_train.py
@@ -364,8 +364,7 @@ class SIIMTrain:
                 iou = iou.detach().cpu().numpy().mean()
                 hinge = hinge.detach().cpu().numpy().mean()
                 bce = bce.detach().cpu().numpy().mean()
-                # ce = bce.detach().cpu().numpy().mean()
-                ce = bce.mean()
+                ce = ce.detach().cpu().numpy().mean()
                 loss = loss.detach().cpu().numpy().mean()
 
                 image = image.cpu().numpy()
@@ -392,6 +391,7 @@ class SIIMTrain:
                             'IOU/{}'.format(config.fold): iou,
                             'Hinge/{}'.format(config.fold): hinge,
                             'BCE/{}'.format(config.fold): bce,
+                            'CE/{}'.format(config.fold): ce,
                             'LogitsProbability/{}'.format(config.fold): logits_predict.mean(),
                             'PredictProbability/{}'.format(config.fold): prob_predict.mean(),
                             'EmptyProbability/{}'.format(config.fold): prob_empty.mean(),
@@ -468,6 +468,7 @@ def eval_fold(net, writer, validation_loader):
             iou = denoised_siim_dice(threshold=config.EVAL_THRESHOLD, iou=True, denoised=False)(labels, prob_predict)
             hinge = lovasz_hinge(labels.squeeze(1), prob_predict.squeeze(1))
             bce = BCELoss(reduction='none')(prob_empty, empty)
+            ce = BCELoss(reduction='none')(prob_predict.squeeze(1).view(prob_predict.shape[0], -1), labels.squeeze(1).view(labels.shape[0], -1))
             loss = 0.5 * dice.mean() + 0.5 * bce.mean()
 
             """DETATCH WITHOUT MEAN"""
@@ -475,6 +476,7 @@ def eval_fold(net, writer, validation_loader):
             iou = iou.detach().cpu().numpy()
             hinge = hinge.detach().cpu().numpy()
             bce = bce.detach().cpu().numpy()
+            ce = ce.detach().cpu().numpy()
             loss = loss.detach().cpu().numpy()
 
             image = image.cpu().numpy()
@@ -509,13 +511,13 @@ def eval_fold(net, writer, validation_loader):
             tensorboardwriter.write_memory(writer, "train")
             # TODO
             if config.DISPLAY_VISUALIZATION and batch_index < max(1, config.MODEL_BATCH_SIZE / 32):
-                for i, (image_, label_, prob_predict_, empty_, prob_empty_, dice_, bce_) in enumerate(zip(image, labels, prob_predict, empty, prob_empty, dice, bce)):
-                    F = draw_image(image_, label_, prob_predict_, empty_, prob_empty_, dice_, bce_)
+                for i, (image_, label_, prob_predict_, empty_, prob_empty_, dice_, bce_, _ce) in enumerate(zip(image, labels, prob_predict, empty, prob_empty, dice, bce, ce)):
+                    F = draw_image(image_, label_, prob_predict_, empty_, prob_empty_, dice_, bce_, ce_)
                     tensorboardwriter.write_image(writer, "{}-{}".format(config.fold, i), F, config.epoch)
 
             """CLEAN UP"""
             del ids, image_0, labels_0  # things threw away
-            del dice, iou, hinge, bce, loss, image, labels, empty, logits_predict, prob_predict, prob_empty  # detach
+            del dice, iou, hinge, bce, ce, loss, image, labels, empty, logits_predict, prob_predict, prob_empty  # detach
             if config.TRAIN_GPU_ARG: torch.cuda.empty_cache()
             if config.DEBUG_TRAISE_GPU: gpu_profile(frame=sys._getframe(), event='line', arg=None)
         del pbar
@@ -569,7 +571,7 @@ def eval_fold(net, writer, validation_loader):
     return score
 
 
-def draw_image(image, ground, pred, empty, prob_empty, dice, bce):
+def draw_image(image, ground, pred, empty, prob_empty, dice, bce, ce):
     F = plt.figure()
 
     plt.subplot(321)
@@ -584,7 +586,7 @@ def draw_image(image, ground, pred, empty, prob_empty, dice, bce):
 
     plt.subplot(323)
     plt.imshow(np.squeeze(pred))
-    plt.title("B:{}".format(bce))
+    plt.title("B:{} C:{}".format(bce, ce))
     plt.grid(False)
 
     return F

utils/lr_finder.py
@@ -177,7 +177,8 @@ class LRFinder(object):
         # siim
         self.optimizer.zero_grad()
         empty_logits, _idkwhatthisis_, logits_predict = self.model(inputs)
-        loss = self.criterion(labels, logits_predict).mean()
+        prob_predict = torch.nn.Sigmoid()(logits_predict)
+        loss = self.criterion(labels, prob_predict).mean()
 
 
         # Backward pass
@@ -205,7 +206,8 @@ class LRFinder(object):
                 # loss = self.criterion(labels, outputs).mean()
                 # siim
                 empty_logits, _idkwhatthisis_, logits_predict = self.model(inputs)
-                loss = self.criterion(labels, logits_predict).mean()
+                prob_predict = torch.nn.Sigmoid()(logits_predict)
+                loss = self.criterion(labels, prob_predict).mean()
 
                 running_loss += loss.item() * inputs.size(0)
 