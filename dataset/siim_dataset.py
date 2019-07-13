938e6a2e19677a5c0ca922b4a558ce6267d53d56
Hanke Chen <i@kokecacao.me>
2019-07-13 10:26:40 -0400

    :book: remove outdated imports; change collate to default collate

--- dataset/siim_dataset.py
+++ dataset/siim_dataset.py
@@ -315,7 +315,7 @@ def train_collate(batch):
         if config.global_steps[config.fold] == 1: print(id, image_0.shape, labels_0.shape)
         new_batch.append(transform(id, image_0, labels_0, empty, mode="train"))
     batch = new_batch
-    return collate(batch)
+    return default_collate(batch)
 
 
 def val_collate(batch):
@@ -323,7 +323,7 @@ def val_collate(batch):
     for id, image_0, labels_0, empty in batch:
         new_batch.append(transform(id, image_0, labels_0, empty, mode="val"))
     batch = new_batch
-    return collate(batch)
+    return default_collate(batch)
 
 
 def test_collate(batch):
@@ -331,7 +331,7 @@ def test_collate(batch):
     for id, image_0, labels_0, empty in batch:
         new_batch.append(transform(id, image_0, labels_0, empty, mode="test"))
     batch = new_batch
-    return collate(batch)
+    return default_collate(batch)
 
 
 def tta_collate(batch):
@@ -339,41 +339,7 @@ def tta_collate(batch):
     for id, image_0, labels_0, empty in batch:
         new_batch.append(transform(id, image_0, labels_0, empty, mode="tta"))
     batch = new_batch
-    return collate(batch)
-
-
-def collate(batch):
-    error_msg = "batch must contain tensors, numbers, dicts or lists; found {}"
-    elem_type = type(batch[0])
-    if isinstance(batch[0], torch.Tensor):
-        out = None
-        return torch.stack(batch, 0, out=out)
-    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
-            and elem_type.__name__ != 'string_':
-        elem = batch[0]
-        if elem_type.__name__ == 'ndarray':
-            # array of string classes and object
-            if re.search('[SaUO]', elem.dtype.str) is not None:
-                raise TypeError(error_msg.format(elem.dtype))
-
-            return torch.stack([torch.from_numpy(b) for b in batch], 0)
-        if elem.shape == ():  # scalars
-            py_type = float if elem.dtype.name.startswith('float') else int
-            return numpy_type_map[elem.dtype.name](list(map(py_type, batch)))
-    elif isinstance(batch[0], int_classes):
-        return torch.LongTensor(batch)
-    elif isinstance(batch[0], float):
-        return torch.DoubleTensor(batch)
-    elif isinstance(batch[0], string_classes):
-        return batch
-    elif isinstance(batch[0], collections.Mapping):
-        return {key: default_collate([d[key] for d in batch]) for key in batch[0]}
-    elif isinstance(batch[0], collections.Sequence):
-        transposed = zip(*batch)
-        return [default_collate(samples) for samples in transposed]
-
-    raise TypeError((error_msg.format(type(batch[0]))))
-
+    return default_collate(batch)
 
 def transform(ids, image_0, labels_0, empty, mode):
     REGULARIZATION_TRAINSFORM = transforms.Compose([
