94fcd742af52780b169c5df22cf83ddb0c3a7b5f
Hanke Chen <i@kokecacao.me>
2019-07-14 00:39:03 -0400

    :tada: I had to resize to fit GPU

--- dataset/siim_dataset.py
+++ dataset/siim_dataset.py
@@ -306,6 +306,7 @@ def eval_aug(term):
 def test_aug(term):
     return Compose([
         HorizontalFlip(p=term % 2),
+        Resize(config.AUGMENTATION_RESIZE_CHANGE, config.AUGMENTATION_RESIZE_CHANGE, interpolation=cv2.INTER_CUBIC),  # 1344
     ])
 
 def tta_aug(term):
@@ -320,6 +321,7 @@ def tta_aug(term):
                 RandomPercentCrop(0.9, 0.9),
                 DoNothing(p=1),
             ], p=1),
+            Resize(config.AUGMENTATION_RESIZE_CHANGE, config.AUGMENTATION_RESIZE_CHANGE, interpolation=cv2.INTER_CUBIC),  # 1344
         ])
 
 
