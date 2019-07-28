6bd3d512cf43635ac86d82bd7a630f9304ef779f
Hanke Chen <i@kokecacao.me>
2019-07-28 14:58:35 -0400

    :tada: change loss to segmentation weighted bce; add BalanceClassSampler

---
+++ Sampler/BalanceClassSampler.py
@@ -0,0 +1,26 @@
+import numpy as np
+from torch.utils.data import Sampler
+
+# https://www.kaggle.com/c/siim-acr-pneumothorax-segmentation/discussion/101429
+class BalanceClassSampler(Sampler):
+    def __init__(self, indices, label, length=None, replace=True):
+        self.indices = indices
+        self.label = label
+        self.replace = replace
+
+        if length is None: length = len(indices)
+        self.length = length
+
+    def __iter__(self):
+        pos_index = np.where(self.label==1)[0]
+        neg_index = np.where(self.label==0)[0]
+        half = self.length//2 + 1
+        pos = np.random.choice(pos_index, half, replace=self.replace)
+        neg = np.random.choice(neg_index, half, replace=self.replace)
+        l = np.stack([pos,neg]).T
+        l = l.reshape(-1)
+        l = l[:self.length]
+        return iter(l)
+
+    def __len__(self):
+        return self.length
