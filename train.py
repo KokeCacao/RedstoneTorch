566482e59466f9f835a7ad3e7383ab3e98dad7ee
Hanke Chen <i@kokecacao.me>
2019-07-28 23:30:49 -0400

    :tada: change to SGD, resetlr now also modify MODEL_INIT_LEARNING_RATE and MODEL_LR_SCHEDULER_BASELR; add train ratio to 5; add logger

--- train.py
+++ train.py
@@ -132,6 +132,7 @@ if __name__ == '__main__':
     if not config.DEBUG_TEST_CODE:
         if config.DEBUG_TRAISE_GPU: sys.settrace(gpu_profile)
 
+        writer = SummaryWriter(config.DIRECTORY_CHECKPOINT)
         log = Logger()
         config.log = log
         if not os.path.exists(config.DIRECTORY_CHECKPOINT + 'log.train.txt'):
@@ -142,7 +143,6 @@ if __name__ == '__main__':
 
         config.log.write('\n--- [START %s] %s\n' % (config.time_to_str(config.start_time), '-' * 64))
 
-        writer = SummaryWriter(config.DIRECTORY_CHECKPOINT)
         config.log.write("=> Tensorboard: " + "tensorboard --logdir=" + config.DIRECTORY_CHECKPOINT + " --port=6006")
 
         reproduceability()
