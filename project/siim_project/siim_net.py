cfb64b23018a14edc6efe2ed68fa9e435c29d622
Hanke Chen <i@kokecacao.me>
2019-08-04 14:16:50 -0400

    :tada: reduce parameters

--- project/siim_project/siim_net.py
+++ project/siim_project/siim_net.py
@@ -658,14 +658,14 @@ class model34_DeepSupervion_GroupNorm_OC(nn.Module):
         #                             nn.LeakyReLU(inplace=True),
         #                             nn.Conv2d(64, 1, kernel_size=1, padding=0))
 
-        # self.logits_final = nn.Sequential(nn.Conv2d(320+64, 64, kernel_size=3, padding=1),
-        #                                  nn.LeakyReLU(inplace=True),
-        #                                  nn.Conv2d(64, 1, kernel_size=1, padding=0))
+        self.logits_final = nn.Sequential(nn.Conv2d(320+64, 64, kernel_size=3, padding=1),
+                                         nn.LeakyReLU(inplace=True),
+                                         nn.Conv2d(64, 1, kernel_size=1, padding=0))
 
-        self.logits_final = nn.Sequential(nn.Conv2d(320+64, 128, kernel_size=3, padding=0),
-                                          ocnet.ASP_OC_Module(128, 48),
-                                          nn.LeakyReLU(inplace=True),
-                                          nn.Conv2d(48, 1, kernel_size=1, padding=0))
+        # self.logits_final = nn.Sequential(nn.Conv2d(320+64, 64, kernel_size=3, padding=1),
+        #                                   ocnet.ASP_OC_Module(64, 48),
+        #                                   nn.LeakyReLU(inplace=True),
+        #                                   nn.Conv2d(48, 1, kernel_size=1, padding=0))
 
     def forward(self, x, flip):
         conv2 = self.conv2(self.conv1(x)) #1/4
